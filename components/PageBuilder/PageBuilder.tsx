5c491ac51e7010a925eac68c007e31c57eae5ca4
root <root@server1.cartandget.com>
2026-02-04 17:16:54 +0000

    WIP: description

--- components/PageBuilder/PageBuilder.tsx
+++ components/PageBuilder/PageBuilder.tsx
@@ -5,6 +5,7 @@ import { CSS } from '@dnd-kit/utilities';
 import { v4 as uuidv4 } from 'uuid';
 import toast from 'react-hot-toast';
 import { uploadPreparedImageToServer } from '../../services/imageUploadService';
+import ComponentLibrary from './components/ComponentLibrary';
 
 // Constants
 const BROKEN_IMAGE_PLACEHOLDER = 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="100" height="100"%3E%3Crect fill="%23ddd" width="100" height="100"/%3E%3Ctext fill="%23999" x="50%25" y="50%25" text-anchor="middle" dominant-baseline="middle"%3EBroken Image%3C/text%3E%3C/svg%3E';
